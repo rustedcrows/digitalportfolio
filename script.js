28fa6d4f1456d9436792afd25feb35421cf3640c
Emily Odam <st33lrocks@gmail.com>
2022-05-13 19:07:14 -0700

    dsdgfgdugeu

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //const Ryan = "Brazil"
 
+class Slider {
+    constructor(sliderElem) {
+        this.slider = sliderElem;
+        this.sliderItems = sliderElem.getElementsByClassName("slider-item");
+        this.nextBtn = sliderElem.querySelector(".slider-control-next");
+        this.prevBtn = sliderElem.querySelector(".slider-control-prev");
+        this.currentIndex = 0;
+        this.prevItemIndex = this.sliderItems.length - 1;
+        this.nextItemIndex = 1;
+        this.isSliding = false;
+    }
+}
+
