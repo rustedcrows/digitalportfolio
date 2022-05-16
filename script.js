7d777317781dac71fcb26f7f9ce5f6a414a71faa
Emily Odam <emily.odam@student.cart.org>
2022-05-15 21:54:45 -0700

    pain

--- script.js
+++ script.js
@@ -1,34 +1,14 @@
 //const Ryan = "Brazil"
-const slider = document.querySelector('.slider');
-const left = document.querySelector('.left');
-const right = document.querySelector('.right');
 
+const buttons = documentQuerySelectorAll ('');
+buttons.forEach(btn => {
+  btn.addEventListener('click', function(e){
+    let x = e.clientX - e.target.offsetLeft
+    let y = e.clientY - e.target.offsetTop
 
-
-const indicatorParent = document.querySelector('.control ul'); 
-const indicators = document.querySelectorAll('.control li');
-var sectionIndex = 0;
-
-indicators.forEach((indicator, i) => {
-  indicator.addEventListener('click', () => {
-    document.querySelector('.control .selected').classList.remove('selected');
-    indicator.classList.add('selected');
-    slider.style.transform = 'translateX(' + (i) * -25 + '%)';  
-    index = i;
-    
-  });
-});
-
-left.addEventListener('click', function() {
-  index = (index > 0) ? index -1 : 0;
-  document.querySelector('.control .selected').classList.remove('selected');
-  indicatorParent.children[index].classList.add('selected');
-  slider.style.transform = 'translateX(' + (index) * -25 + '%)';
-});
-
-right.addEventListener('click', function() {
-  index = (index < 4 - 1) ? index+1 : 3;
-  document.querySelector('.control .selected').classList.remove('selected');
-  indicatorParent.children[index].classList.add('selected');
-  slider.style.transform = 'translateX(' + (index) * -25 + '%)';
-});
+    let ripples = document.createElement('span');
+    ripples.style.left = x + 'px';
+    ripples.style.top = y + 'px';
+    this.appendChild(ripples);
+  })
+})
